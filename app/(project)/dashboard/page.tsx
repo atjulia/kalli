46ed21a56a443d312d01abef15b9e24d9beff666
Julia Karolina <juliakarolina24@gmail.com>
2025-04-16 12:39:53 -0300

    authentication with auth.js and integration with firebase

---
+++ app/(project)/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { auth } from "@/app/lib/auth";
+import { redirect } from "next/navigation";
+import { handleAuth } from "@/app/actions/handle-auth"
+
+export default async function Dashboard() {
+	const session = await auth();
+
+	console.log(session)
+	if (!session) {
+		redirect("/login");
+	}
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="text-4xl font-bold">Protected dashboard</h1>
+			<p>{session?.user?.email ? session?.user?.email : "Usuário não está logado"}</p>
+			{session?.user?.email && (
+				<form action={handleAuth}>
+					<button type="submit" className="border rounded-md px-3 cursor-pointer">Logout</button>
+				</form>
+			)}
+		</div>
+	);
+}
+  
